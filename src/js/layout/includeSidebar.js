b7907ef4b0f5959921181bbdab6861d8fa7526e1
whozbebop <whozbebop@gmail.com>
2025-10-14 11:17:23 +0900

    update 초기 세팅

---
+++ src/js/layout/includeSidebar.js
@@ -0,0 +1,5 @@
+fetch("/layout/sidebar.html")
+  .then( (response) => response.text() )
+  .then( (responseText) => {
+    document.querySelector(".main-container").insertAdjacentHTML("afterbegin", responseText);
+  } )
