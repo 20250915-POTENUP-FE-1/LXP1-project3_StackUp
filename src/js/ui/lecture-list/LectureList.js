54adc2ad2a8acf0f9052258dd7d51fe0fffc9432
christina-ahn <ysahn@tlksys.com>
2025-10-16 09:43:57 +0900

    feat(ui): refactor component/css/html (#23)

---
+++ src/js/ui/lecture-list/LectureList.js
@@ -0,0 +1,7 @@
+// fetch("/src/js/ui/lecture-list/lecture-list.html")
+//   .then((res) => res.text())
+//   .then((resText) => {
+//     document
+//       .querySelector("#lecture-list")
+//       .insertAdjacentHTML("afterbegin", resText);
+//   });
